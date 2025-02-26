cc3364aa123b8ec2b426b95fbc579e09a7da5b21
Ростислав <64661692+Lokiner890@users.noreply.github.com>
2025-02-26 20:36:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,4 @@ document.addEventListener("DOMContentLoaded", function () {
             musicPlayerContainer.classList.remove('is-playing');
         }
     });
-
-    // Добавление эффекта "лампового мерцания" при игре музыки
-    setInterval(() => {
-        if (musicPlayerContainer.classList.contains('is-playing')) {
-            musicPlayerContainer.style.boxShadow = `5px 5px 20px rgba(0, 0, 0, ${Math.random() * 0.6 + 0.3})`;
-        }
-    }, 500);
 });
